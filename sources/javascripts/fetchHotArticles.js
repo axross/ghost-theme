c4e0f20558331a03a74b294503abcff25ea9fdf1
axross <kou@axross.org>
2015-09-28 00:32:57 +0900

    Add HotArticles

---
+++ sources/javascripts/fetchHotArticles.js
@@ -0,0 +1,66 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const $hotArticles = $('#hotArticles');
+  const partialLiTemplate = `
+    <li class="hotArticles__body__articles__item">
+      <a
+        href=":url"
+        class="hotArticles__body__articles__item__link"
+      >
+        <div
+          class="hotArticles__body__articles__item__link__image"
+          style="background-image: url(:image)"
+        ></div>
+        <div class="hotArticles__body__articles__item__link__meta">
+          <span class="hotArticles__body__articles__item__link__meta__title">
+            <span>
+              :title
+            </span>
+
+            <img
+              src="http://b.hatena.ne.jp/entry/image/:url"
+              alt="はてなブックマーク - :title"
+              title="はてなブックマーク - :title"
+            >
+          </span>
+        </div>
+      </a>
+    </li>
+  `;
+
+  const appendHotArticle = ({ title, url, images }) => {
+    console.log(title, url, images);
+    const partialHtml = partialLiTemplate
+                          .replace(/\:title/g, title)
+                          .replace(/\:url/g, url)
+                          .replace(/\:image/g, images[0]);
+    const $partial = $(partialHtml);
+
+    $hotArticles.append($partial);
+  };
+
+  google.load('feeds', '1', {
+    callback() {
+      // const feed = new google.feeds.Feed(
+      //   `http://b.hatena.ne.jp/entrylist?mode=rss&sort=count&url=${window.location.protocol}//${window.location.hostname}`
+      // );
+      const feed = new google.feeds.Feed(
+        `http://b.hatena.ne.jp/entrylist?mode=rss&sort=count&url=http://axross.me/`
+      );
+      feed.setNumEntries(5);
+      feed.load(result => {
+        if (result.err) return console.error(err);
+
+        result.feed.entries
+          .map(entry => {
+            return {
+              title: entry.title,
+              url: entry.link,
+              images: entry.content.match(/https?:\/\/[^"]+\.(gif|jpg|png)/g),
+              publishedAt: new Date(entry.publishedDate),
+            };
+          })
+          .forEach(entry => appendHotArticle(entry));
+      });
+    },
+  });
+});
